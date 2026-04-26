8d3dcd06830b5ef5f9a5d84de333d7e584524175
Niteshkhosla <ntsh10khosla@gmail.com>
2026-04-27 00:51:20 +0530

    Update rag_setup.py
    Warning ignore

--- rag_setup.py
+++ rag_setup.py
@@ -1,5 +1,5 @@
-from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import SentenceTransformerEmbeddings
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
 
 # Android knowledge base
